99ce8a665add3f74298a6f73ef8043cf0dd130ef
carlosdevice1 <carlosdevice123@gmail.com>
2022-04-22 13:14:55 -0300

    fist

--- src/teste.py
+++ src/teste.py
@@ -5,4 +5,18 @@ ps4 = Produto('Playstation 4', 1789.44)
 xbox = Produto('Xbox 360', 1699.99)
 
 print(ps4,'\n')
+print(xbox)from models.produto import Produto
+
+
+ps4 = Produto('Playstation 4', 1789.44)
+xbox = Produto('Xbox 360', 1699.99)
+
+print(ps4,'\n')
+print(xbox)from models.produto import Produto
+
+
+ps4 = Produto('Playstation 4', 1789.44)
+xbox = Produto('Xbox 360', 1699.99)
+
+print(ps4,'\n')
 print(xbox)
